2d840c1ba3e30cfc68d45b3e83ead86dfe07e97f
Conner Krenzer <krenzer84@gmail.com>
2024-08-03 12:41:28 -0400

    wip

--- frontend/index.js
+++ frontend/index.js
@@ -52,70 +52,71 @@ async function sprintChallenge5() { // Note the async keyword so you can use `aw
 
   // 👇 ==================== TASK 3 START ==================== 👇
 
-  // for (let learner of learners) { // looping over each learner object
+  for (let learner of learners) { // looping over each learner object
 
-  //   // 🧠 Flesh out the elements that describe each learner
-  //   // ❗ Give the elements below their (initial) classes, textContent and proper nesting.
-  //   // ❗ Do not change the variable names, as the code that follows depends on those names.
-  //   // ❗ Also, loop over the mentors inside the learner object, creating an <li> element for each mentor.
-  //   // ❗ Fill each <li> with a mentor name, and append it to the <ul> mentorList.
-  //   // ❗ Inspect the mock site closely to understand what the initial texts and classes look like!
+    // 🧠 Flesh out the elements that describe each learner
+    // ❗ Give the elements below their (initial) classes, textContent and proper nesting.
+    // ❗ Do not change the variable names, as the code that follows depends on those names.
+    // ❗ Also, loop over the mentors inside the learner object, creating an <li> element for each mentor.
+    // ❗ Fill each <li> with a mentor name, and append it to the <ul> mentorList.
+    // ❗ Inspect the mock site closely to understand what the initial texts and classes look like!
 
-  //   const card = document.createElement('div')
-  //   const heading = document.createElement('h3')
-  //   const email = document.createElement('div')
-  //   const mentorsHeading = document.createElement('h4')
-  //   const mentorsList = document.createElement('ul')
+    const card = document.createElement('div')
+    card.classList.add("card")
+    const heading = document.createElement('h3')
+    const email = document.createElement('div')
+    const mentorsHeading = document.createElement('h4')
+    const mentorsList = document.createElement('ul')
 
-  //   // 👆 ==================== TASK 3 END ====================== 👆
+    // 👆 ==================== TASK 3 END ====================== 👆
 
-  //   // 👆 WORK ONLY ABOVE THIS LINE 👆
-  //   // 👆 WORK ONLY ABOVE THIS LINE 👆
-  //   // 👆 WORK ONLY ABOVE THIS LINE 👆
-  //   card.appendChild(mentorsList)
-  //   card.dataset.fullName = learner.fullName
-  //   cardsContainer.appendChild(card)
+    // 👆 WORK ONLY ABOVE THIS LINE 👆
+    // 👆 WORK ONLY ABOVE THIS LINE 👆
+    // 👆 WORK ONLY ABOVE THIS LINE 👆
+    card.appendChild(mentorsList)
+    card.dataset.fullName = learner.fullName
+    cardsContainer.appendChild(card)
 
-  //   card.addEventListener('click', evt => {
-  //     const mentorsHeading = card.querySelector('h4')
-  //     // critical booleans
-  //     const didClickTheMentors = evt.target === mentorsHeading
-  //     const isCardSelected = card.classList.contains('selected')
-  //     // do a reset of all learner names, selected statuses, info message
-  //     document.querySelectorAll('.card').forEach(crd => {
-  //       crd.classList.remove('selected')
-  //       crd.querySelector('h3').textContent = crd.dataset.fullName
-  //     })
-  //     info.textContent = 'No learner is selected'
-  //     // conditional logic
-  //     if (!didClickTheMentors) {
-  //       // easy case, no mentor involvement
-  //       if (!isCardSelected) {
-  //         // selecting the card:
-  //         card.classList.add('selected')
-  //         heading.textContent += `, ID ${learner.id}`
-  //         info.textContent = `The selected learner is ${learner.fullName}`
-  //       }
-  //     } else {
-  //       // clicked on mentors, we toggle and select no matter what
-  //       card.classList.add('selected')
-  //       if (mentorsHeading.classList.contains('open')) {
-  //         mentorsHeading.classList.replace('open', 'closed')
-  //       } else {
-  //         mentorsHeading.classList.replace('closed', 'open')
-  //       }
-  //       if (!isCardSelected) {
-  //         // if card was not selected adjust texts
-  //         heading.textContent += `, ID ${learner.id}`
-  //         info.textContent = `The selected learner is ${learner.fullName}`
-  //       }
-  //     }
-  //   })
-  // }
+    card.addEventListener('click', evt => {
+      const mentorsHeading = card.querySelector('h4')
+      // critical booleans
+      const didClickTheMentors = evt.target === mentorsHeading
+      const isCardSelected = card.classList.contains('selected')
+      // do a reset of all learner names, selected statuses, info message
+      document.querySelectorAll('.card').forEach(crd => {
+        crd.classList.remove('selected')
+        crd.querySelector('h3').textContent = crd.dataset.fullName
+      })
+      info.textContent = 'No learner is selected'
+      // conditional logic
+      if (!didClickTheMentors) {
+        // easy case, no mentor involvement
+        if (!isCardSelected) {
+          // selecting the card:
+          card.classList.add('selected')
+          heading.textContent += `, ID ${learner.id}`
+          info.textContent = `The selected learner is ${learner.fullName}`
+        }
+      } else {
+        // clicked on mentors, we toggle and select no matter what
+        card.classList.add('selected')
+        if (mentorsHeading.classList.contains('open')) {
+          mentorsHeading.classList.replace('open', 'closed')
+        } else {
+          mentorsHeading.classList.replace('closed', 'open')
+        }
+        if (!isCardSelected) {
+          // if card was not selected adjust texts
+          heading.textContent += `, ID ${learner.id}`
+          info.textContent = `The selected learner is ${learner.fullName}`
+        }
+      }
+    })
+  }
 
-  // const footer = document.querySelector('footer')
-  // const currentYear = new Date().getFullYear()
-  // footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+  const footer = document.querySelector('footer')
+  const currentYear = new Date().getFullYear()
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
 }
 
 // ❗ DO NOT CHANGE THIS CODE. WORK ONLY INSIDE TASKS 1, 2, 3
